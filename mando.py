1b6449019aecfa4a3f1b9a09f0083870033fc05e
Vasu Singhal <vasusinghal0403@gmail.com>
2020-01-16 21:46:33 +0530

    improved how the player looks

--- mando.py
+++ mando.py
@@ -2,20 +2,41 @@ from board_obj import Board_obj
 from player import Player
 from playarea import Playarea
 import sys
+from colorama import Fore, Back, Style
+
 class Mando:
+	# player_cords = {
+	# 	12 : [10, 11, 12],
+	# 	13 : [10, 11, 12],
+	# 	14 : [11],
+	# 	15 : [11],
+	# }
 	player_cords = {
-		12 : [10, 11, 12],
-		13 : [10, 11, 12],
-		14 : [11],
-		15 : [11],
+		12: [13, 14, 15],
+		13: [10, 11, 12, 13, 14, 15],
+		14: [10, 11, 12, 13, 14, 15],
+		15: [10, 11, 12, 13, 14, 15],
+		16: [10, 12, 13, 14, 15],
+		17: [10, 12, 13, 15]
+	}
+	player_disp = {
+		0: ["<", ")", ")"],
+		1: [Back.RED + " " + Style.RESET_ALL, Back.RED + " " + Style.RESET_ALL, Back.RED + " " + Style.RESET_ALL, "<", ")", ")"],
+		2: [Back.RED + " " + Style.RESET_ALL, Back.RED + " " + Style.RESET_ALL, Back.RED + " " + Style.RESET_ALL, Back.CYAN + " " + Style.RESET_ALL, Back.CYAN + " " + Style.RESET_ALL, Back.CYAN + " " + Style.RESET_ALL],
+		3: [Back.RED + " " + Style.RESET_ALL, Back.RED + " " + Style.RESET_ALL, Back.RED + " " + Style.RESET_ALL, Back.CYAN + " " + Style.RESET_ALL, Back.CYAN + " " + Style.RESET_ALL, Back.CYAN + " " + Style.RESET_ALL],
+		4: ["|", "|", Back.CYAN + " " + Style.RESET_ALL, Back.CYAN + " " + Style.RESET_ALL, Back.CYAN + " " + Style.RESET_ALL],
+		5: ["|", "|", Back.BLUE + " " + Style.RESET_ALL, Back.BLUE + " " + Style.RESET_ALL]
 	}
 	life = 3
 	score = 0
 	def insert_into_grid(self, grid):
+		cnt1 = 0
 		for i in self.player_cords:
+			cnt2 = 0
 			for j in self.player_cords[i]:
-				grid[i][j] = Player()
-
+				grid[i][j] = Player(self.player_disp[cnt1][cnt2])
+				cnt2 += 1
+			cnt1 += 1
 	def move_mando(self, player_column, movement, pos, grid, fire_beams):
 		new_player_cords = {}
 		for i in self.player_cords:
@@ -46,13 +67,17 @@ class Mando:
 				grid[i][j] = Playarea()
 
 		self.player_cords.clear()
+		cnt1 = 0
 		for i in new_player_cords:
 			self.player_cords[i] = []
+			cnt2 = 0
 			for j in new_player_cords[i]:
 				if grid[i][j].points != 0:
 					self.score += 1
-				grid[i][j] = Player()
+				grid[i][j] = Player(self.player_disp[cnt1][cnt2])
+				cnt2 += 1
 				self.player_cords[i].append(j)
+			cnt1 += 1
 		return player_column
 
 	def gravity(self, player_column, pos, grid, fire_beams):
--- player.py
+++ player.py
@@ -2,7 +2,7 @@ from board_obj import Board_obj
 from colorama import Fore, Back, Style
 
 class Player(Board_obj):
-	def __init__(self):
+	def __init__(self, disp):
 		Board_obj.__init__(self)
-		self.disp = Back.RED + " " + Style.RESET_ALL
+		self.disp = disp
 		self.player = 1
